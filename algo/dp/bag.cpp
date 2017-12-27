68b65a941daa3ae687e1e487371e147f3caf7be5
cloud <920317005@qq.com>
2017-12-27 23:14:40 +0800

    bag

--- algo/dp/bag.cpp
+++ algo/dp/bag.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-int main() {
+void Solve01Bag() {
      int num = 4;
      int max_weight = 15;
 
@@ -13,38 +13,24 @@ int main() {
      int tmp_val[] ={0,10,3,1,2}; // 价值
      vector<int> w(tmp_w , tmp_w + 5);
      vector<int> val(tmp_val, tmp_val + 5);
-     
-     vector<vector<int> > array(num + 1, vector<int>(max_weight, 0)); 
-     for (int i = 0; i < array.size(); i++) { 
-         for (int j = 0; j < array[i].size();j++)
-         {
-             cout << array[i][j] << ",";
-         }
-         cout << endl;
-     }
 
-     // array[i][j] 前i个物品，重量为j时，最大价值
+     vector<int> first(max_weight, 0);
      // i: 1-num 
      // j: 背包重量 0 - max_weight
      for(int i = 1; i <= num; i++) { // 物品
-         for(int j = 0; j <= max_weight; j++) { //
+         for(int j = max_weight; j >= 0; j--) { //
              if (j >= w[i]) {
-                 array[i][j] = max(array[i-1][j], array[i-1][j - w[i]] + val[i]);
+		 		first[j] = max(first[j], first[j-w[i]] + val[i]); 
              } 
-             else {
-                 array[i][j] = array[i-1][j];
-             }
          }
      }
-
      cout << "----------------" << endl;
-     for (int i = 0; i < array.size(); i++) { 
-         for (int j = 0; j < array[i].size();j++)
-         {
-             cout << array[i][j] << ",";
-         }
-         cout << endl;
-     }
-     return -1;
+	 typedef ostream_iterator<int> ostream_iter;
+	 copy(first.begin(), first.end(), ostream_iter(cout, " "));
+}
+
+int main() {
+	Solve01Bag();
+	return -1;
 }
 
