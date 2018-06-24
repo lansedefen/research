fb79e5d032844d5247cb4c7b223c69ff92814071
cloud <920317005@qq.com>
2018-06-24 23:46:44 +0800

    xx

--- algo/list/mergeklist.cpp
+++ algo/list/mergeklist.cpp
@@ -3,42 +3,37 @@
 #include<queue>  
 #include<set>  
 #include <functional> // std::greater  
+#include"list_head.h"
 
 using namespace std;  
   
-struct cmp  
-{  
-    bool operator()(ListNode* a, ListNode* b)  
-    {  
-        return a->val > b->val;  
+struct cmp {  
+    bool operator()(ListNode* a, ListNode* b) {  
+        return a->value > b->value;  
     }  
 };  
 
-//方法一：利用最小堆方法  
 //用一个大小为K的最小堆（用优先队列+自定义降序实现）(优先队列就是大顶堆，队头元素最大,自定义为降序后，就变成小顶堆，队头元素最小)，  
 //先把K个链表的头结点放入堆中，每次取堆顶元素，然后将堆顶元素所在链表的下一个结点加入堆中。  
-//
-ListNode* mergeKLists2(vector<ListNode*> lists)  
-{  
-    if (lists.size() == 0) return NULL;  
-    priority_queue<int, vector<ListNode*>, cmp> heap;  
-    for (int i = 0; i < lists.size(); ++i)  
-    {  
+ListNode* mergeKLists2(vector<ListNode*> lists)  {  
+    if (lists.size() == 0) 
+		return NULL;  
+
+    priority_queue<ListNode*, vector<ListNode*>, cmp> heap;  
+    for (int i = 0; i < lists.size(); ++i)  {  
         heap.push(lists[i]);  
     }  
   
-    ListNode* newHead=NULL;  
-    ListNode* p=NULL;  
-    ListNode* q=NULL;  
-    while (!heap.empty())  
-    {  
+    ListNode* new_head, *p, *q;
+	new_head = p = q = NULL;
+    while (!heap.empty())  {  
         q = heap.top();  
         heap.pop();  
-        if (q->next != NULL)    
+        if (q->next)    
             heap.push(q->next);
 
-        if (newHead == NULL) {   
-            newHead = q;  
+        if (!new_head) {   
+            new_head = q;  
             p = q;  
         }  
         else  
@@ -47,24 +42,23 @@ ListNode* mergeKLists2(vector<ListNode*> lists)
             p = p->next;  
         }  
     }  
-    return newHead;  
+    return new_head;  
 }  
   
-int main()  
-{  
+int main()  {  
     vector<ListNode*> lists;  
 
     int array_1[5] = {1,2,3,4,5};
     vector<int> input_array_1(array_1, array_1+4);
     ListNode* head_1;
-    CreateList(&head_1, input_array);
+    CreateList(&head_1, input_array_1);
     cout << "链表l1： ";  
     PrintList(head_1);
 
     int array_2[5] = {2,4,6,7,9};
     vector<int> input_array_2(array_2, array_2+4);
     ListNode* head_2;
-    CreateList(&head_2, input_array);
+    CreateList(&head_2, input_array_2);
     cout << "链表l2： ";  
     PrintList(head_2);
 
@@ -74,10 +68,9 @@ int main()
     ListNode* res = mergeKLists2(lists);  
     cout << "合并后链表： ";  
     while (res) {  
-        cout << res->val << " ";  
+        cout << res->value << " ";  
         res = res->next;  
     }  
     cout << endl;  
-    DestroyList(res);  
     return 0;  
 }  
