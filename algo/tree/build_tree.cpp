269627d6273382e25ca50c53731716c49c2aef8a
lansedefen <920317005@qq.com>
2018-06-26 15:29:15 +0800

    xx

--- algo/tree/build_tree.cpp
+++ algo/tree/build_tree.cpp
@@ -17,6 +17,23 @@ TreeNode* CopyTree(TreeNode* root) {
     return new_root;
 }
 
+TreeNode* RebuildTree(string input, int& index) {
+
+    cout << "id:" << index << " str:" <<  input[index] <<endl;
+    if (input[index] == '#' ) {
+        return NULL;
+    }
+    
+    TreeNode* new_root = new TreeNode(int(input[index]) - 48 );
+    index = index +1;
+    TreeNode* left_child = RebuildTree(input, index);
+    index = index +1;
+    TreeNode* right_child = RebuildTree(input, index);
+    new_root->left = left_child;
+    new_root->right = right_child;
+    return new_root;
+}
+
 int main() {
     TreeNode* root;
 	BuildTree(root);
@@ -25,5 +42,8 @@ int main() {
     TreeNode* new_root = CopyTree(root);
     PrintTree(new_root);
 
+    int index = 0;
+    TreeNode* new_root2 = RebuildTree("12##3##", index); 
+    PrintTree(new_root2);
     return -1;	
 }
