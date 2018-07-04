5add0b02aa399bce2bc554ec0cf6d301bd86eb36
lansedefen <920317005@qq.com>
2018-07-04 19:38:51 +0800

    xx

--- algo/sort/quick_sort.cpp
+++ algo/sort/quick_sort.cpp
@@ -1,28 +1,25 @@
 #include<iostream>
 #include<vector>
 #include"array_head.h"
+#include"../list/list_head.h"
 
 using namespace std;
 
 int PartVector(vector<int>& arr, int start, int end) {
 	int i = start, j = end;
 	int base = arr[start];
-    cout << "start:" << start << ", end:" << end << ", base:" << base << endl;
-    PrintVector(arr, "arr1");
 	while(i < j) {
 	    while (arr[j] >= base && i < j) {
 	        j--;
 	    }
 	    arr[i] = arr[j];
 
-        while (arr[i] <= base && i < j) {
+        while (arr[i] < base && i < j) {
 	        i++;
 	    }
 	    arr[j] = arr[i];
 	}
-	arr[start] = base;
-
-    PrintVector(arr, "arr2");
+	arr[i] = base;
     return i;
 }
 
@@ -32,8 +29,40 @@ void QuickSort(vector<int>& arr, int start, int end) {
 	}
 
     int middle = PartVector(arr, start, end);
-	QuickSort(arr, start, middle - 1);
+	QuickSort(arr, start, middle);
 	QuickSort(arr, middle + 1, end);
+}
+
+ListNode* PartList(ListNode* head, ListNode* start, ListNode* end) {
+    int base = head->value;
+    cout << "base:" << base << endl;
+
+    /*
+    start = head;
+    end = left->next;
+    while(right) {
+        if(base >= right->data) {
+            left = left->next;
+            int tmp = right->data;
+            right->data = left->data;
+            left->data = tmp;
+        }   
+        right = right->next;
+    }
+    head->data = left->data;
+    left->data = base;
+*/
+    return head;
+}
+
+void QuickSortList(ListNode* head, ListNode* start, ListNode* end) {
+    if(head == NULL) {
+        return;
+    }
+    ListNode* middle = PartList(head, start, end);
+    QuickSortList(head, start, middle);
+    
+    
 }
 
 int main() {
