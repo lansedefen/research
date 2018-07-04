b457ca2a1036e9a814f2983d30b918f27812ab8e
cloud <920317005@qq.com>
2018-07-04 22:41:38 +0800

    xx

--- algo/sort/combine_sort.cpp
+++ algo/sort/combine_sort.cpp
@@ -11,16 +11,18 @@ void CombineVector(vector<int>& arr, int start, int middle, int end) {
       int i = start, j = middle + 1;
       int index = start;
       while( i <= middle || j <= end) {
-          if(j > end || tmp[i] < tmp[j]) {
+		  cout << "i:" << i << ", j:" << j << ", tmp_i:" << tmp[i] <<", tmp_j:"<< tmp[j] << ", middle:" << middle << ", end:" << end << endl;
+          if(j > end || (tmp[i] < tmp[j] && i <= middle)) {
 	        arr[index++] = tmp[i++];
             continue;
 	      } 
 
-          if (i > middle || tmp[i] >= tmp[j])  {
+          if (i > middle || (tmp[i] >= tmp[j] && j <= end))  {
 	        arr[index++] = tmp[j++];
             continue;
 	      }
       }
+	  PrintVector(arr, "arr1");
 }
 
 void MergeSort(vector<int>& arr, int start, int end) {
@@ -31,12 +33,14 @@ void MergeSort(vector<int>& arr, int start, int end) {
      MergeSort(arr, start, middle);
      MergeSort(arr, middle + 1, end);
      CombineVector(arr, start, middle, end);
-
 }
 
 int main() {
-    int nums[] = {-1, 10, -1, 6, 5, 0};
-    vector<int> arr(nums, nums + 6);
+    //int nums[] = {-1, 10, -1, 6, 5, 0};
+	//vector<int> arr(nums, nums + 6);
+
+    vector<int> arr;
+	CreateVector(arr);
     PrintVector(arr, "arr");
     MergeSort(arr, 0, arr.size()-1);
     PrintVector(arr, "arr" );
