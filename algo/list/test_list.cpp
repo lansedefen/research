f0490be1f98dbaca09513040722c65c096be2bad
lansedefen <920317005@qq.com>
2018-07-19 01:19:26 +0800

    xx

--- algo/list/test_list.cpp
+++ algo/list/test_list.cpp
@@ -5,38 +5,13 @@
 
 using namespace std;
 
-void move(ListNode* &p) {
-    p = p->next;
-}
-
-void RemoveNode2(ListNode* &p, int value) {
-    if (!p) {
-        return;
-    }
-
-    if (p->value == value) {
-        move(p);
-        return;
-    }
-
-    ListNode* q = p;
-    while(q) {
-        if(q->next->value == value ) {
-                move(q->next);
-                break;
-        } 
-        q = q->next;
-    }
-}
-
 int main() {
-    int a[5] = {1,2,3,4,5};
+    int a[5] = {1,2,3,2,5};
     vector<int> input_array(a, a+5);
     ListNode* head;
     CreateList(&head, input_array);
     PrintList(head);
-    RemoveNode2(head, 2);
-
+    DeleteListNode(head, 5);
     PrintList(head);
 
     /*ListNode* end = GetListEnd(head);
