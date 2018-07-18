c8f557bb4b0ee75bf7d46051bf1bb59ffa71a2d4
lansedefen <920317005@qq.com>
2018-07-19 01:17:34 +0800

    xx

--- algo/list/remove_list.h
+++ algo/list/remove_list.h
@@ -8,8 +8,32 @@
 
 using namespace std;
 
+void move(ListNode* &p) {
+    p = p->next;
+}
+
+void RemoveNode(ListNode* &p, int value) {
+    if (!p) {
+        return;
+    }
+
+    if (p->value == value) {
+        move(p);
+        return;
+    }
+
+    ListNode* pre = p;
+    while(pre->next) {
+        if(pre->next->value == value ) {
+                move(pre->next);
+                continue;
+        } 
+        pre = pre->next;
+    }
+}
+
 ListNode* PartList(ListNode* start, ListNode* end) {
-    ListNode* index = start;
+    /*ListNode* index = start;
     int base = start->value;
     while(start != end) {
         if(start->value < value) {
@@ -18,7 +42,7 @@ ListNode* PartList(ListNode* start, ListNode* end) {
 
         cout << start-> value << ", ";
         start = start->next;
-    }
+    }*/
     return start;
 }
 
