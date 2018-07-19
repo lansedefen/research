745980e4b96f86f90f786363243b707a7d39e24b
lansedefen <920317005@qq.com>
2018-07-19 20:12:51 +0800

    xx

--- algo/sort/heap.cpp
+++ algo/sort/heap.cpp
@@ -3,106 +3,79 @@
 #include <vector>
 #include <numeric>
 #include <algorithm>
+#include"../array/array_head.h" 
 
 using namespace std;
 
-void InsertHeapElement(vector<int>& heap, int value) {
+void InsertHeap(vector<int>& heap, int value) {
+    int i = heap.size();
     heap.push_back(value);
-    int i = heap.size();
-    cout << i << endl;
+
     while(i >0) {
-       if ((i-1)/2 > 0 && heap[i] < heap[(i-1)/2]) {
-           int tmp = heap[i];
-           heap[i] = heap[(i-1)/2];
-           heap[(i-1)/2] = tmp;
+       int father_index = (i-1)/2;
+       if (father_index > 0 && heap[i] < heap[father_index]) {
+            swap(heap[i], heap[father_index]);
        }
-       i = (i-1)/2;
+       i = father_index;
     }
 }
 
-int DeleteHeap(vector<int>& heap) {
-    int value = heap[0];
-    heap[0] = value;
-    heap.pop();
-
-    int i = heap.size();
-    cout << i << endl;
-    while(i >0) {
-       if ((i-1)/2 > 0 && heap[i] < heap[(i-1)/2]) {
-           int tmp = heap[i];
-           heap[i] = heap[(i-1)/2];
-           heap[(i-1)/2] = tmp;
-       }
-       i = (i-1)/2;
-    }
-    return value;
-}
-
-void adjust(vector<int>& heap, int index, int value) {
+void AdjustHeap(vector<int>& heap, int index, int value) {
       int len = heap.size();
       heap[index] = value;
       int i = index;
       while (i<len) {  
          int min_index = i;
+         int left_index = 2*i+1;
+         int right_index = 2*i+2;
+
          // 先和左子树比较，获取较小值
-         if(2*i + 1 < len) {
-             if(heap[i] > heap[2*i +1]) {
-                 min_index = 2*i +1;
-             }
+         if(left_index < len && heap[i] > heap[left_index]) {
+                 min_index = left_index;
          } 
          // 右边
-         if(2*i +2 < len) { // 
-             if(heap[min_index] > heap[2*i +2]) {
-                min_index = 2*i +2;
-             }
+         if(right_index < len && heap[min_index] > heap[right_index]) {
+                min_index = right_index;
          }
 
-         if (min_index == i) {
-             return;
+         if (i == min_index) {
+            return;
          }
-
-         int tmp = heap[min_index];
-         heap[min_index] = heap[i];
-         heap[i] = tmp;
+         swap(heap[i], heap[min_index]);
          i = min_index;
       }
 }
 
-void build(vector<int>& heap) {
+int DeleteHeap(vector<int>& heap, int& value) {
+    if (heap.size() == 0) {
+        return -1;
+    }
+
+    value = heap[0];
+    heap[0] = heap.back();
+    heap.pop_back();
+
+    AdjustHeap(heap, 0, value);
+    return value;
+}
+
+
+void BuildHeap(vector<int>& heap) {
    int len = heap.size();
     for (int i = len/2; i--; i>=0) {
-        adjust(heap, i, heap[i]);
+        AdjustHeap(heap, i, heap[i]);
     }
 }
 
 int main() {
-    //1 build nlog(n)
-    /* 
-    vector<int> heap;
-    insert(heap, 1);
-    insert(heap, 3);
-    insert(heap, 6);
-    insert(heap, 8);
-    cout << "----" << endl;
-    for (int i=0; i< heap.size(); i++) {
-        cout << heap[i] << endl;
-    }
-    */
-
     // 2 build log(n)
     vector<int> heap;
     heap.push_back(3);
     heap.push_back(6);
     heap.push_back(1);
     heap.push_back(8);
-    build(heap); 
+    BuildHeap(heap); 
     cout << "----" << endl;
-    for (int i=0; i< heap.size(); i++) {
-        cout << heap[i] << endl;
-    }
-
-    cout << "----" << endl;
-    adjust(heap,0, 4);
     for (int i=0; i< heap.size(); i++) {
         cout << heap[i] << endl;
     }
