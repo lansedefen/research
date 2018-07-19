f85c5b33f37cfd3b58929e2c06e015fc781e31e9
lansedefen <920317005@qq.com>
2018-07-20 04:34:52 +0800

    xx

--- algo/list/test_list.cpp
+++ algo/list/test_list.cpp
@@ -6,21 +6,17 @@
 using namespace std;
 
 int main() {
-    int a[5] = {1,2,3,2,5};
+    int a[5] = {4,2,3,6,5};
     vector<int> input_array(a, a+5);
     ListNode* head;
     CreateList(&head, input_array);
     PrintList(head);
-    DeleteListNode(head, 5);
-    PrintList(head);
+    //DeleteListNode(head, 5);
+    //PrintList(head);
 
-    /*ListNode* end = GetListEnd(head);
-    cout << end->value << endl;
-    ListNode* middle = GetListMiddle(head);
-    cout << middle->value << endl;
-
-    PartList(head, end);
-    */
+    ListNode* end = GetListEnd(head);
+    ListNode* new_head = PartList(head, NULL);
+    PrintList(new_head);
 
     return -1;    
 }
