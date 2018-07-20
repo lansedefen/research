729611a0a068a4aba0dd8cdac4ec8b2eb7845a92
cloud <920317005@qq.com>
2018-07-20 22:23:35 +0800

    xx

--- algo/sort/winner_tree.cpp
+++ algo/sort/winner_tree.cpp
@@ -5,32 +5,43 @@
 using namespace std;
 
 void AdjustTree(vector<int>& tree, vector<int>& leaves, int i) {  
- int K = leaves.size();
- int m, n;  
- if(2 * i < K)               /* 获取它的左孩子结点 */  
-     m = tree[2 * i + 1];  
- else  
-     m = 2 * i - K + 1;  
-
- if(2 * i + 1 < K)                 /* 获取它的右孩子节点 */  
-     n = tree[2*i + 2];  
- else  
-     n = 2 * i + - K + 2;  
-
- tree[i] = leaves[m] > leaves[n] ? n : m; /* 进行胜负判定 */  
+	int K = leaves.size();
+	int m, n;
+	int left_index = 2*i + 1;
+	if( left_index < K - 1)               /* 获取它的左孩子结点 */  
+		m = tree[left_index];  
+	else  
+		m = left_index - (K - 1);  
+ 
+	int right_index = 2*i + 2;
+	if(right_index < K -1)                 /* 获取它的右孩子节点 */  
+		n = tree[right_index];  
+	else  
+		n = right_index - (K - 1);  
+	tree[i] = leaves[m] > leaves[n] ? m : n; /* 进行胜负判定 */  
 }  
 
 void BuildTree(vector<int>& tree, vector<int>& leaves)  {  
- tree.clear();
- for(int i = tree.size()-1; i>=0; i--)  
-     AdjustTree(tree, leaves, i);  
+	tree.resize(leaves.size() - 1);
+	for(int i = tree.size()-1; i>=0; i--)  
+        AdjustTree(tree, leaves, i);  
 }  
+
+void DeleteTree(vector<int>& tree, vector<int>& leaves, int& value)  {  
+	value = leaves[tree[0]];
+	swap(leaves.back(), leaves[tree[0]]);
+	leaves.pop_back();
+	BuildTree(tree, leaves); 
+}
 
 int main()  {  
     int nums[] = {3, 6, 1, 8, 9};
     vector<int> leaves(nums, nums + 5);
-    vector<int> tree(0, 5);
-    BuildTree(leaves, tree); 
-    PrintVector(leaves, "before");    
+    vector<int> tree;
+
+    PrintVector(leaves, "leaves");    
+    BuildTree(tree, leaves); 
+    PrintVector(tree, "tree");    
+
     return 0;  
 }  
