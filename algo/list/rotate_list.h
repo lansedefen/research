de874725df885a9b21efa767a16e75be591498f9
lansedefen <920317005@qq.com>
2018-08-01 03:41:30 +0800

    xx

--- algo/list/rotate_list.h
+++ algo/list/rotate_list.h
@@ -1,3 +1,21 @@
 #include<vector>
 #include"list_head.h"
 
+ListNode* RotateList(ListNode* head, int n) {
+    if(!head) return head;
+ 
+    ListNode new_head = NULL;
+    ListNode p = head;
+    int i=0;
+    while(!p && i< n-1) {
+        new_head = p->next;
+        p = p ->next;
+        i++;
+    }
+
+    while(!p->next) {
+        p = p -> next;
+    }
+    p->next = head;
+    return new_head;
+}
