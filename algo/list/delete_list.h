70351e161b6a96b0556cd0ec1ea348af5e300430
lansedefen <920317005@qq.com>
2018-08-01 18:53:19 +0800

    xx

---
+++ algo/list/delete_list.h
@@ -0,0 +1,35 @@
+#ifndef REMOVE_LIST_H_                                                
+#define REOMOVE_LIST_H_ 
+
+
+#include<iostream>
+#include<vector>
+#include"list_head.h"
+
+using namespace std;
+
+void move(ListNode* &p) {
+    p = p->next;
+}
+
+void DeleteListNode(ListNode* &p, int value) {
+    if (!p) {
+        return;
+    }
+
+    if (p->value == value) {
+        move(p);
+        return;
+    }
+
+    ListNode* pre = p;
+    while(pre->next) {
+        if(pre->next->value == value ) {
+                move(pre->next);
+                continue;
+        } 
+        pre = pre->next;
+    }
+}
+
+#endif
--- algo/list/move_list.h
+++ algo/list/move_list.h
@@ -8,30 +8,6 @@
 
 using namespace std;
 
-void move(ListNode* &p) {
-    p = p->next;
-}
-
-void DeleteListNode(ListNode* &p, int value) {
-    if (!p) {
-        return;
-    }
-
-    if (p->value == value) {
-        move(p);
-        return;
-    }
-
-    ListNode* pre = p;
-    while(pre->next) {
-        if(pre->next->value == value ) {
-                move(pre->next);
-                continue;
-        } 
-        pre = pre->next;
-    }
-}
-
 ListNode* PartList(ListNode* start, ListNode* end) {
     ListNode* index = start;
     int base = start->value;
